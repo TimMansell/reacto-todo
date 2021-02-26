3727a2f6c1f63fefe22ae34ba46c624a2fd17ecf
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-02-26 11:39:43 +1100

    refactor to use classnames

--- src/components/TodoRemoveCompletedItems.js
+++ src/components/TodoRemoveCompletedItems.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import classnames from 'classnames';
 import { clearCompletedTodos } from '../store/todo';
 import PropTypes from 'prop-types';
 
 export const TodoRemoveCompletedItems = ({ remainingTodos }) => {
   const dispatch = useDispatch();
 
+  const classes = classnames({
+    hidden: remainingTodos,
+  });
+
   return (
     <div className="text-center">
       <br />
-      <span className={remainingTodos ? '' : 'hidden'}>
+      <span className={classes}>
         <button
           className="p-2 bg-green-500 text-white rounded-md"
           onClick={() => dispatch(clearCompletedTodos())}
